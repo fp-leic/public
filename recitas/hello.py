cbd164239e94adb8732b0a02c2ffe46b74b9c936
Correia Lopes <jlopes@fe.up.pt>
2021-09-30 15:52:08 +0100

    Added recitas/hello.py

---
+++ recitas/hello.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 13 18:07:57 2019
+@author: jlopes
+"""
+
+# my first variable
+greeting = "Hello"
+
+#
+# COMMENTING LINES #######
+#
+# to comment MANY lines at a time, highlight all of them then CTRL+1
+# do CTRL+1 again to uncomment them
+# try it on the next two commented lines below!
+
+#whoami = "FIXME"
+#greeting = greeting + " " + whoami
+
+# output the greeting
+print("\n" + greeting + "!")
