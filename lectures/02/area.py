5e82d4759cfef151a37fdb690fe40d8fe8610eb6
Correia Lopes <jlopes@fe.up.pt>
2021-10-10 23:05:41 +0100

    added lectures/02/ code examples

---
+++ lectures/02/area.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 26 10:10:54 2018
+@author: jlopes, from: "How to Think Like a Computer Scientist"
+"""
+
+## first without composition
+#response = input("What is your radius? ")
+#r = float(response)
+#area = 3.14159 * r**2
+#print("The area is", area)
+
+## now let's use composition
+#r = float( input("What is your radius? ") )
+#print("The area is", 3.14159 * r**2)
+
+## All in one statement
+#print("The area is", 3.14159*float(input("What is your radius? " ))**2)
---
+++ lectures/02/remainder.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 26 10:48:29 2018
+@author: jlopes, from: "How to Think Like a Computer Scientist"
+"""
+
+#total_secs = int(input("How many seconds, in total? "))
+#hours = total_secs // 3600
+#secs_still_remaining = total_secs % 3600
+#minutes = secs_still_remaining // 60
+#secs_finally_remaining = secs_still_remaining % 60
+#
+#print("Hrs=", hours, "mins=", minutes, "secs=", secs_finally_remaining)
