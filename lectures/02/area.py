a72e241340cfbea16a9c816b37eb0d02e41dda69
João Correia Lopes <jlopes60@users.noreply.github.com>
2021-10-12 16:21:17 +0100

    Update area.py

--- lectures/02/area.py
+++ lectures/02/area.py
@@ -5,15 +5,18 @@ Created on Wed Sep 26 10:10:54 2018
 @author: jlopes, from: "How to Think Like a Computer Scientist"
 """
 
-## first without composition
-#response = input("What is your radius? ")
-#r = float(response)
+## 1. first without composition
+
+#r = float(input("What is your radius? "))
 #area = 3.14159 * r**2
 #print("The area is", area)
 
-## now let's use composition
+## 2, now let's use composition
+
 #r = float( input("What is your radius? ") )
 #print("The area is", 3.14159 * r**2)
 
-## All in one statement
+## 3. All in one statement
 #print("The area is", 3.14159*float(input("What is your radius? " ))**2)
+
+# the last is not easiest to understand, is it?
