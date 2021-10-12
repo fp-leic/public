b8297f2a3e0490e1165c20916fc3d86846b82c41
João Correia Lopes <jlopes60@users.noreply.github.com>
2021-10-12 10:26:13 +0100

    Update remainder.py

--- lectures/02/remainder.py
+++ lectures/02/remainder.py
@@ -5,10 +5,11 @@ Created on Wed Sep 26 10:48:29 2018
 @author: jlopes, from: "How to Think Like a Computer Scientist"
 """
 
-#total_secs = int(input("How many seconds, in total? "))
-#hours = total_secs // 3600
-#secs_still_remaining = total_secs % 3600
-#minutes = secs_still_remaining // 60
-#secs_finally_remaining = secs_still_remaining % 60
-#
-#print("Hrs=", hours, "mins=", minutes, "secs=", secs_finally_remaining)
+total_secs = int(input("How many seconds, in total? "))
+
+hours = total_secs // 3600
+ecs_still_remaining = total_secs % 3600
+minutes = secs_still_remaining // 60
+secs_finally_remaining = secs_still_remaining % 60
+
+print("Hrs=", hours, "mins=", minutes, "secs=", secs_finally_remaining)
