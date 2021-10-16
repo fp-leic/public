6d1b3fa18565817c02e39f2d4263c69debb64f44
Correia Lopes <jlopes@fe.up.pt>
2021-10-16 16:43:26 +0100

    modified: 03/simplify.py

--- lectures/03/simplify.py
+++ lectures/03/simplify.py
@@ -13,28 +13,32 @@ Created on Thu Oct  4 19:17:59 2018
 sword_charge = 0.95
 shield_energy = 110
 
-#if not (sword_charge >= 0.90 and shield_energy >= 100):
-#    print("\nYour attack has no effect, the dragon fries you to a crisp!")
-#else:
-#    print("\nThe dragon crumples in a heap. You rescue the gorgeous princess!")
+######################
+# NO simplification
+######################
+
+if not (sword_charge >= 0.90 and shield_energy >= 100):
+    print("\nYour attack has no effect, the dragon fries you to a crisp!")
+else:
+    print("\nThe dragon crumples in a heap. You rescue the gorgeous princess!")
 
 ######################
 # 1st simplification
 ######################
 
-#if sword_charge < 0.90 or shield_energy < 100:
-#    print("\nYour attack has no effect, the dragon fries you to a crisp!")
-#else:
-#    print("\nThe dragon crumples in a heap. You rescue the gorgeous princess!")
+if sword_charge < 0.90 or shield_energy < 100:
+    print("\nYour attack has no effect, the dragon fries you to a crisp!")
+else:
+    print("\nThe dragon crumples in a heap. You rescue the gorgeous princess!")
 
 ######################
 # 2nd simplification
 ######################
 
-#if sword_charge >= 0.90 and shield_energy >= 100:
-#    print("\nThe dragon crumples in a heap. You rescue the gorgeous princess!")
-#else:
-#    print("\nYour attack has no effect, the dragon fries you to a crisp!")
+if sword_charge >= 0.90 and shield_energy >= 100:
+    print("\nThe dragon crumples in a heap. You rescue the gorgeous princess!")
+else:
+    print("\nYour attack has no effect, the dragon fries you to a crisp!")
 
 ######################
 # 3rd simplification
