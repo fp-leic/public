dd8b013a1221191e43746e306e99c46985db0917
João Correia Lopes <jlopes60@users.noreply.github.com>
2021-10-17 18:41:04 +0100

    Update guess.py

--- lectures/04/guess.py
+++ lectures/04/guess.py
@@ -11,8 +11,8 @@ Peter Wentworth, Jeffrey Elkner, Allen B. Downey, and Chris Meyers,
 How to Think Like a Computer Scientist — Learning with Python 3, 2012
 """
 
-import random          # We cover random numbers in the modules chapter
-rng = random.Random()  # "rng" stands for "random number generator"
+import random                    # We cover random numbers in the modules chapter
+rng = random.Random()            # "rng" stands for "random number generator"
 number = rng.randrange(1, 1000)  # Get random number between (1 and 1000).
 
 guesses = 0
@@ -27,4 +27,4 @@ while True:
         message += str(guess) + " is too low.\n"
     else:
         break
-input("\nGreat, you got it in "+str(guesses)+" guesses!\n")
+print("\nGreat, you got it in "+str(guesses)+" guesses!\n")
