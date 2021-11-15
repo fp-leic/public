9efc8a47dd6c163872bb300134d03dd9bc22f2ac
Correia Lopes <jlopes@fe.up.pt>
2021-11-15 15:40:57 +0000

    Added NB10 with slides

--- lectures/07/tmore.py
+++ lectures/07/tmore.py
@@ -65,8 +65,8 @@ print(tup1)
 
 ### Built-in Tuple Functions
 
-#tuple1, tuple2 = (123, 'xyz', 'zara', 'abc'), (456, 700, 200)
+#tuple1, tuple2 = ('123', 'xyz', 'zara', 'abc'), (456, 700, 200)
 #
 #print()
-##print("Max value element : ", max(tuple1))
+#print("Max value element : ", max(tuple1))
 #print("Max value element : ", max(tuple2))
