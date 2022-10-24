9e57740f183ddf11fa0b16b01b68faeb40d910fd
Correia Lopes <jlopes@fe.up.pt>
2022-10-24 15:46:40 +0100

    renamed 03/prob_quadratic.py

--- lectures/03/prob_quadratic.py
+++ lectures/03/prob_quadratic.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Oct  6 11:36:24 2022
+Created on Thu Oct  4 11:36:24 2022
 
 @author: jlopes
 """
 
+"""PROBLEM
+
 # Find the roots of quadratic equation ax^2 + bx + c = 0
 # were a, b and c are real numbers and a <> 0
+https://www.geeksforgeeks.org/python-program-to-solve-quadratic-equation/
+"""
 
 import math
 
