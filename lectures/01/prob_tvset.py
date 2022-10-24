0778c795aa72d2d73cee650ee5c804367adda3c3
Correia Lopes <jlopes@fe.up.pt>
2022-10-24 15:30:44 +0100

    added lectures/01/prob_tvset.py

---
+++ lectures/01/prob_tvset.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 09 15:27:15 2022
+
+@author: jlopes
+"""
+
+"""
+PROBLEM:
+If the sale price of a big tv set is 1000€ and the shop gives back the
+VAT (IVA) how much do you pay?
+"""
+
+sell_price = 1000.00    # start by 1000
+IVA = 0.23              # it does NOT go to "Variable explorer" ?!
+U = 1                   # goes to  "Variable explorer"
+price = sell_price / (U + IVA)
+print(round(price, 2))  # round it as currency
