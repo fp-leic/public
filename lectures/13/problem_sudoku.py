cb32c85f295bf3672b5b23b19bddeac2f6a1f025
Pedro Vasconcelos <pbv@dcc.fc.up.pt>
2022-11-25 08:55:56 +0000

    remove globals

--- lectures/13/problem_sudoku.py
+++ lectures/13/problem_sudoku.py
@@ -13,20 +13,19 @@
 #
 
 # An example Sudoku puzzle
-grid = [[5,3,0,0,7,0,0,0,0],
-        [6,0,0,1,9,5,0,0,0],
-        [0,9,8,0,0,0,0,6,0],
-        [8,0,0,0,6,0,0,0,3],
-        [4,0,0,8,0,3,0,0,1],
-        [7,0,0,0,2,0,0,0,6],
-        [0,6,0,0,0,0,2,8,0],
-        [0,0,0,4,1,9,0,0,5],
-        [0,0,0,0,8,0,0,1,9]]
+example_grid = [[5,3,0,0,7,0,0,0,0],
+                [6,0,0,1,9,5,0,0,0],
+                [0,9,8,0,0,0,0,6,0],
+                [8,0,0,0,6,0,0,0,3],
+                [4,0,0,8,0,3,0,0,1],
+                [7,0,0,0,2,0,0,0,6],
+                [0,6,0,0,0,0,2,8,0],
+                [0,0,0,4,1,9,0,0,5],
+                [0,0,0,0,8,0,0,1,9]]
 
 
-def check_possible(number,row,col):
+def check_possible(grid,number,row,col):
     '''Check if it is valid to place number at coordinates row,col.'''
-    global grid
     # search row 
     for i in range(0,9):
         if grid[row][i] == number:
@@ -47,18 +46,16 @@ def check_possible(number,row,col):
     # all other cases: possible
     return True
 
-def print_grid():
+def print_grid(grid):
     '''Print the current state of the grid.'''
-    global grid
     print("Solution:")
     for row in grid:
         print(row)   
     print(40*'-')
 
 
-def solve():
+def solve(grid):
     '''Solve the grid, printing all possible solutions.'''
-    global grid
     # list of positions to fill in
     zeros = []
     for row in range(9):
@@ -66,15 +63,14 @@ def solve():
             if grid[row][col] == 0:
                 zeros.append((row,col))
     # fill in recursively
-    solve_rec(zeros)
+    solve_rec(grid,zeros)
         
 
-def solve_rec(zeros):
+def solve_rec(grid, zeros):
     '''Worker function that solves the puzzle recursively.'''
-    global grid
     if len(zeros) == 0:
         # finished; just print the solution
-        print_grid()
+        print_grid(grid)
     else:
         # first zero position
         (row,col) = zeros[0]
@@ -82,9 +78,14 @@ def solve_rec(zeros):
         rest = zeros[1:]
         for number in range(1,10):
             # check if we can place number at row,col 
-            if check_possible(number,row,col):
+            if check_possible(grid,number,row,col):
                 # place number and recursively continue
                 grid[row][col] = number
-                solve_rec(rest)
+                solve_rec(grid,rest)
                 # undo the placement to try other alternatives
                 grid[row][col] = 0
+
+
+# ---------------------------------------------
+# Solve the example grid
+solve(example_grid)
